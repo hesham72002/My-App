9f690b3f41a88de5cef36cd4e61c78150eedcbe5
Hesham Rajab <hesham.7.r@gmail.com>
2023-03-02 15:29:26 +0200

    sec

--- app/src/main/java/com/hshm/myapplication/MainActivity.kt
+++ app/src/main/java/com/hshm/myapplication/MainActivity.kt
@@ -4,26 +4,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.hshm.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var lngList: ArrayList<String>
+    lateinit var callRV: RecyclerView
+
+    val listAdapter = ListAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val db = Firebase.firestore
 
-        binding.button.setOnClickListener {
-            var text1 = binding.editTextTextPersonName.text.toString()
-            var text2 = binding.editTextTextPersonName2.text.toString()
-            var text3 = binding.editTextTextPersonName3.text.toString()
+        binding.btnAdd.setOnClickListener {
+            var name = binding.name.text.toString()
+            var phone = binding.phone.text.toString()
+            var address = binding.address.text.toString()
             // Create a new user with a first and last name
             val user = hashMapOf(
-                "text1" to text1,
-                "text2" to text2,
-                "text3" to text3
+                "name" to name,
+                "phone" to phone,
+                "address" to address
             )
 
             db.collection("users")
@@ -36,6 +43,27 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "no secs", Toast.LENGTH_SHORT).show()
                     Log.w("hshm", "Error adding document", e)
                 }
+            listAdapter.addItem(name,phone,address)
+            listAdapter.notifyItemChanged(changingConfigurations)
+
+        }
+//        binding.btnRemove.setOnClickListener {
+//            Toast.makeText(applicationContext, "Remove Item", Toast.LENGTH_SHORT).show()
+//            listAdapter.removeItem()
+//            listAdapter.notifyItemChanged(changingConfigurations)
+//
+//        }
+
+        binding.recyclerview01.apply {
+            adapter = listAdapter
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+
+        binding.refreshLayout01.setOnRefreshListener {
+            listAdapter.notifyDataSetChanged()
+            Toast.makeText(applicationContext, "List Refresh", Toast.LENGTH_SHORT).show()
+            binding.refreshLayout01.isRefreshing = false
         }
 
     }
